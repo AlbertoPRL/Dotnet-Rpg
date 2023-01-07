00011cfde7ac5b532e87f578f5da111bcec775a3
Alberto <apablorojaslopez@gmail.com>
2023-01-07 11:58:41 -0500

    Adding ServerResponse

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -21,19 +21,19 @@ namespace dotnet_rpg.Controllers
             
         }
         [HttpGet("GetAll")]
-        public async Task<ActionResult<List<Character>>> Get()
+        public async Task<ActionResult<ServiceResponse<List<Character>>>> Get()
         {
             return Ok(await _characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]//THE PARAMETER ROUTE SHOULD MATCH THE PARAMATER NAME ON THE FUNCTION ITSELF AND SHOULD BE BETWEEN CURLY BRACES
-        public async Task<ActionResult<Character>> GetSingle(int id)
+        public async Task<ActionResult<ServiceResponse<Character>>> GetSingle(int id)
         {
             return Ok(await _characterService.GetCharacterById(id));
         }
 
         [HttpPost]
-        public async Task<ActionResult<List<Character>>> addCharacter(Character newCharacter)
+        public async Task<ActionResult<ServiceResponse<List<Character>>>> addCharacter(Character newCharacter)
         {
 
             
